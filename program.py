44f82946f18cf667152be99c8534105fed4a8913
misharopopo-star <misharopopo@gmail.com>
2025-12-25 11:09:17 +0700

    Create program.py

---
+++ program.py
@@ -0,0 +1,11 @@
+# main.py
+def main():
+    print("Hello from CI/CD!")
+    result = add(5, 3)
+    print(f"5 + 3 = {result}")
+
+def add(a, b):
+    return a + b
+
+if name == "__main__":
+    main()
