0f7e65bc0783b9fb72802ffbd08d0a5c3e1679e7
misharopopo-star <misharopopo@gmail.com>
2025-12-25 12:02:00 +0700

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,12 +5,17 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("🎯 ПРИВЕТ! CI/CD ПАЙПЛАЙН ЗАПУЩЕН И РАБОТАЕТ");
+        Console.WriteLine("🎯 ПРИВЕТ! CI/CD РАБОТАЕТ!");
         Console.WriteLine($"Сейчас: {DateTime.Now}");
         
+        // Запуск тестов
+        SimpleTests.RunTests();
+        
+        // Основная логика
         int sum = Add(10, 20);
         Console.WriteLine($"10 + 20 = {sum}");
     }
     
-    static int Add(int a, int b) => a + b;
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
 }
